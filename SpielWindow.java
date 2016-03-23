29c8d8d3f1d87e5b7064c8b4f02829a0747513b6
Tim Austin <tja.xpprime@gmail.com>
2016-03-22 20:46:35 -0600

    Update SpielWindow.java

--- SpielWindow.java
+++ SpielWindow.java
@@ -1,16 +1,23 @@
 import javax.swing.JFrame;
 import javax.swing.Timer;
+import java.util.Random;
 
 public class SpielWindow extends JFrame{
+  
+  Random rand = new Random();
+  
+  //constructor
   public SpielWindow() {
     super();
     
     setTitle("Spiel");
-    setSize(620, 410);
+    setSize(600, 410);
     
-    Spiel content = new Spiel();
+    Spiel content = new Spiel(1, 140, 573, 140, rand.nextInt(460) + 160, rand.nextInt(330) + 120, 2, 3);
+    getContentPane().add(content);
     
     addMouseListener(content);
     addKeyListener(content);
+    
   }
 }
