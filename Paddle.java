f9c09f4f7c42f07df094b133c4e94aa3dec088b5
Tim Austin <tja.xpprime@gmail.com>
2016-03-22 20:48:59 -0600

    Update Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,5 +1,24 @@
-import javax.swing.JOptionPane;
-
 public class Paddle{
+  //member variables
+  private int xPaddle;
+  private int yPaddle;
   
+  //constructor
+  public Paddle(int xPaddle, int yPaddle) {
+    this.xPaddle = xPaddle;
+    this.yPaddle = yPaddle;
+  }
+  
+  //member methods
+  public void setYPaddle(int newY) {
+    yPaddle = newY;
+  }
+  
+  public int getXPaddle() {
+    return xPaddle;
+  }
+  
+  public int getYPaddle() {
+    return yPaddle;
+  }
 }
